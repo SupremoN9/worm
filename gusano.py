ec9062efdc8274acbcf09f6e093780566f246d65
SupremoN9 <115044099+SupremoN9@users.noreply.github.com>
2023-01-02 02:12:03 -0600

    Update gusano.py

--- gusano.py
+++ gusano.py
@@ -27,7 +27,7 @@ for i in range(0, 99):
     directorio = 'spo'+str(i)
     subprocess.call(['mkdir',directorio])
 subprocess.call(['cp',name,directorio])
-subprocess.call(['mv',name,directorio])
+
 # SupremoN9
 
 from sys import argv 
@@ -41,7 +41,6 @@ for i in range(0, 99):
     directorio = 'nas'+str(i)
     subprocess.call(['mkdir',directorio])
 subprocess.call(['cp',name,directorio])
-subprocess.call(['mv',name,directorio])
 
 
 # SupremoN9
@@ -57,7 +56,6 @@ for i in range(0, 99):
     directorio = 'spo3qw'+str(i)
     subprocess.call(['mkdir',directorio])
 subprocess.call(['cp',name,directorio])
-subprocess.call(['mv',name,directorio])
 
 
 
