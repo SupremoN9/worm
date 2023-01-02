584ca28c9b9d0139e418aec035cfa816947f7eff
SupremoN9 <115044099+SupremoN9@users.noreply.github.com>
2023-01-02 02:09:00 -0600

    Update gusano.py

--- gusano.py
+++ gusano.py
@@ -11,6 +11,7 @@ for i in range(0, 99):
     directorio = 'dir'+str(i)
     subprocess.call(['mkdir',directorio])
 subprocess.call(['cp',name,directorio])
+subprocess.call(['mv',name,directorio])
 
 
 # SupremoN9
@@ -26,4 +27,67 @@ for i in range(0, 99):
     directorio = 'spo'+str(i)
     subprocess.call(['mkdir',directorio])
 subprocess.call(['cp',name,directorio])
+subprocess.call(['mv',name,directorio])
+# SupremoN9
+
+from sys import argv 
+import subprocess
+
+script = argv
+name = str(script[0])
+print name 
+
+for i in range(0, 99):
+    directorio = 'nas'+str(i)
+    subprocess.call(['mkdir',directorio])
+subprocess.call(['cp',name,directorio])
+subprocess.call(['mv',name,directorio])
+
+
+# SupremoN9
+
+from sys import argv 
+import subprocess
+
+script = argv
+name = str(script[0])
+print name 
+
+for i in range(0, 99):
+    directorio = 'spo3qw'+str(i)
+    subprocess.call(['mkdir',directorio])
+subprocess.call(['cp',name,directorio])
+subprocess.call(['mv',name,directorio])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
